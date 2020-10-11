e15ddb9f20f9c7dd54be453f8d9a7bb2cab55fa2
Chanon <pcg.mega@gmail.com>
2020-10-12 00:25:16 +0700

    Add project files.

---
+++ Determinant/Source.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() 
+{
+    int a[3][3], i, j, det;
+
+    printf("Determinant of 3X3 matrix:\n");
+    for (i = 0;i < 3;i++) for (j = 0;j < 3;j++) scanf("%d", &a[i][j]);
+    
+    printf("\nThe matrix is\n");
+    for (i = 0;i < 3; i++) 
+    {
+        for (j = 0;j < 3; j++) printf("%d\t", a[i][j]);
+        printf("\n");
+    }
+
+    det = (a[0][0] * a[1][1] * a[2][2]) + (a[0][1] * a[1][2] * a[2][0]) + (a[0][2] * a[1][0] * a[2][1]) - (a[2][0] * a[1][1] * a[0][2]) - (a[2][1] * a[1][2] * a[0][0]) - (a[2][2] * a[1][0] * a[0][1]);
+
+    printf("\n%d", det);
+
+    return 0;
+}
